dd7d0e60f4c4b803808049db068d7a6d4fa39b02
khrystie <86361095+khrystie@users.noreply.github.com>
2021-06-24 16:35:34 -0300

    Create 04_faixaEtaria_A0.java

---
+++ logicaSelecao/04_faixaEtaria_A0.java
@@ -0,0 +1,37 @@
+/************************************************************************
+	Programa: Leitura da idade, exibe a qual fila pertence de acordo
+	com a faixa etária. 
+  Tag: Estrutura Lógica de Seleção, JOptionPane
+ 	Autor: Chrystie
+ 	Data: 15/09/2020
+************************************************************************/
+
+import javax.swing.JOptionPane;
+
+public class JOag4 {
+
+	public static void main(String[] args) {
+		
+		try {
+		
+			int idade = Integer.parseInt((JOptionPane.showInputDialog("Digite a idade do usuário: ")));
+		
+			if (idade>=60) 
+				if (idade > 80)  
+					JOptionPane.showMessageDialog(null,"Fila 80+");
+				else 
+					JOptionPane.showMessageDialog(null, "Fila prioritária");						
+			else  
+				JOptionPane.showMessageDialog(null, "Fila comum");
+		
+		
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "Apenas números inteiros..", "Eror", JOptionPane.WARNING_MESSAGE);
+		}
+		
+	}
+}
+		
+	
+
+
