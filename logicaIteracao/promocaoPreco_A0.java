be741cb05ba91058f93bbe43c69bc12b663b0fce
khrystie <86361095+khrystie@users.noreply.github.com>
2021-06-24 20:56:29 -0300

    Create promocaoPreco_A0.java

---
+++ logicaIteracao/promocaoPreco_A0.java
@@ -0,0 +1,34 @@
+/************************************************************************
+	Programa: Leitura do nome de dez produtos e seus respectivos preços. 
+	Exibe valor promocional no terminal.
+	Tags: Estrutura Lógica de iteração(Laço de repetição PARA), 
+	JOptionPane, try-catch 
+ 	Autor: Chrystie
+ 	Data: 28/09/2020
+************************************************************************/
+
+import javax.swing.JOptionPane;
+
+public class Chrystie_Ag6_DS_I_JOptionPane {
+	public static void main (String[] args) {
+		// declaração de variáveis
+		String produto;
+		double price;
+		//tratamento de erros
+		try {
+			// laço de repetição para
+			for (int i=1; i<=10; i++) {
+				// entrada de dados
+				produto = JOptionPane.showInputDialog(null,"Produto " +(i+0) +": ");
+				price = Double.parseDouble(JOptionPane.showInputDialog(null,"Preço " +(i+0) +":"));	
+				// saída de dados com processamento 
+				System.out.println("Nome do produto "+(i+0)+": " + produto);
+				System.out.println("Preço promocional: R$" +price*0.50	+"\n");	
+			}
+			
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "Entrar apenas com números", "E r r o", JOptionPane.ERROR_MESSAGE);
+		}
+			
+	}
+}
