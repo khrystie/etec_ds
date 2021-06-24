eae3195940d9cc9cc1e6321b3325529867b67a91
khrystie <86361095+khrystie@users.noreply.github.com>
2021-06-24 14:33:22 -0300

    Create acessoLogin.java

---
+++ logicaSelecao/acessoLogin.java
@@ -0,0 +1,36 @@
+/************************************************************************
+	Programa: Leitura de login e senha. Exibe se acesso é liberado ou não.
+  Tag: Estrutura Lógica de Seleção ou Estrutra de Decisão, Scanner
+ 	Autor: Chrystie
+ 	Data: 12/09/2020
+************************************************************************/
+
+// importa biblioteca
+import java.util.Scanner;
+
+public class ErrorMsg_e_equalString {
+
+	public static void main(String[] args) {
+		// declare
+		String login, senha; 
+		// leitor de dados
+		Scanner leitor = new Scanner (System.in);
+		
+		
+		// entrada de dados
+		System.out.println("Entre com o login");
+		login = leitor.next();
+		System.out.println("Entre com uma senha");
+		senha = leitor.next();
+		
+		// estrutura de decisão
+		if  (login.equals("login") && (senha.equals("senha"))) 
+			//se atender a condição
+			System.out.println("Acesso liberado"); 
+		 else 
+			//caso contrário
+			System.out.println("Acesso não permitido" );
+		
+	} // final do main
+
+} // final da classe
