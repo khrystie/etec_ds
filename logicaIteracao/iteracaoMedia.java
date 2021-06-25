dba3956d1ef598073f4a778d01d562e4824f109f
khrystie <86361095+khrystie@users.noreply.github.com>
2021-06-24 23:04:31 -0300

    Create iteracaoMedia.java

---
+++ logicaIteracao/iteracaoMedia.java
@@ -0,0 +1,31 @@
+/************************************************************************
+	Programa: Entrada de cinco números inteiros. Exibe a média.
+	Tags: Estrutura Lógica de iteração enquanto-faça, JOptionPane
+ 	Autor: Chrystie
+ 	Data: 08/10/2020
+************************************************************************/
+
+import javax.swing.JOptionPane;
+
+public class Chrystie_Ag7_DS_I_apostila_exerc1_dowhile {
+
+	public static void main(String[] args) {
+		// variáveis
+		int num, media, soma=0, i=1;
+					
+		do {
+			// entrada de dados
+			num = Integer.parseInt(JOptionPane.showInputDialog(i +"- Digite um número: "));
+			// processamento
+			// ou soma = soma + num
+			soma += num;
+			// iteração 
+			i++;
+			} while(i<=5);
+			// processamento - cálculo da média
+		media = soma/5;
+		// saída - resultado		
+		JOptionPane.showMessageDialog(null, "A média é: "+media);
+	}
+
+}
