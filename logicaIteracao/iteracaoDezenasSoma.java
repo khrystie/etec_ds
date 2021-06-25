cf77f3a2c101b7deb02a3e962a737faa7ab71023
khrystie <86361095+khrystie@users.noreply.github.com>
2021-06-25 10:25:03 -0300

    Create iteracaoDezenasSoma.java

---
+++ logicaIteracao/iteracaoDezenasSoma.java
@@ -0,0 +1,26 @@
+/************************************************************************
+	Programa: Soma das dezenas, centenas, etc. 
+	Tags: Estrutura Lógica de iteração enquanto, JOptionPane
+ 	Autor: Chrystie
+ 	Data: 08/10/2020
+************************************************************************/
+
+import  javax.swing.JOptionPane;
+public class Chrystie_Ag7_DS_I_apostila_exerc2_while {
+	public static void main(String[] args) {
+		// variáveis	
+		int num=0, i=0, soma;
+		// entrada número	
+		num = Integer.parseInt(JOptionPane.showInputDialog(null,"Insira um número inteiro: "));
+		// processamento
+		soma = num%10;
+		
+		// calcular soma das dezenas, centenas, etc
+		while (num>0) {
+			num /= 10;
+			soma = soma + (num%10);
+		}
+			
+		JOptionPane.showMessageDialog(null,soma);
+	}
+}
