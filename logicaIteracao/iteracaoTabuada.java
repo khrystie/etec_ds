dae4151ed3c10ea6e07ef743a8880178676afa23
khrystie <86361095+khrystie@users.noreply.github.com>
2021-06-25 20:59:30 -0300

    Create iteracaoTabuada.java

---
+++ logicaIteracao/iteracaoTabuada.java
@@ -0,0 +1,26 @@
+/************************************************************************
+	Programa: Exibe a tabuada dos números de 1 a 5.
+	Tags: Estrutura Lógica de iteração enquanto e para
+ 	Autor: Chrystie
+ 	Data: 08/10/2020
+************************************************************************/
+
+
+public class Chrystie_Ag7_DS_I_opcional { 
+	public static void main (String[] args) {
+		// variáveis
+		int numero=1;
+		// laço repetição enquanto
+		// numeros de 1 a 5
+		while (numero<=5) { 
+			// laço repetição para
+			// multiplica cada numero por 1 a 10
+			for (int contador=1; contador<=10; contador++) {
+				int resultado = numero*contador;	
+				System.out.println(+numero+ "*"+ contador +"= " +numero*contador);	
+			}				
+			numero++;	
+		} 
+			
+	}
+}
