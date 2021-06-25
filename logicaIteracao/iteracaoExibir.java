7e91e44741465db70f63cf1e6c245aadf2a57f8d
khrystie <86361095+khrystie@users.noreply.github.com>
2021-06-24 22:34:34 -0300

    Update iteracaoExibir.java

--- logicaIteracao/iteracaoExibir.java
+++ logicaIteracao/iteracaoExibir.java
@@ -1,6 +1,6 @@
 /************************************************************************
-	Programa: Exibe números a cada dez unidades de 0 a 10 e números de 0
-	a 4.
+	Programa: Exibe números a cada dez unidades de 0 a 10 e números 
+	de 0 a 4.
 	Tags: Estrutura Lógica de iteração enquanto e para
  	Autor: Chrystie
  	Data: 28/09/2020
@@ -8,20 +8,18 @@
 
 public class iteracaoExibir {
 	public static void main (String[] args) {
-		// saída mensagem
-    System.out.println("Pula de 10 em 10 até 100 ");
-    // laço de repetição para
+	// saída mensagem
+    		System.out.println("Pula de 10 em 10 até 100 ");
+    		// laço de repetição para
 		for (int cc=0; cc<=100; cc+=10) 
-      // saída números de dez em dez
+      			// saída números de dez em dez
 			System.out.println(cc);
 		
 		// limpa o valor da variavel	
 		int cc=0;
 		// laço de repetição enquanto		
-		while (cc<=4) { 
-      // saída números ++
+		while (cc<=4) 
+      		// saída números ++
 			System.out.println(cc++);	
-		}
-		
 	}
 }
