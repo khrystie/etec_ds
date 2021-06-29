e5b7c7ced54f84050ed25a121c58a0400fdd9578
khrystie <86361095+khrystie@users.noreply.github.com>
2021-06-29 12:04:49 -0300

    Update jsExterno.js

--- JavaScript/jsExterno.js
+++ JavaScript/jsExterno.js
@@ -1,15 +1,14 @@
-/* 
+/*
  	Programa:  exibe números de 0 a 100.
  	Tags: javascript externo, laço de repetição para
  	Data: 05/12/2020
  	Autor: Chrystie
 */
 
-// variáveis
+/* variáveis */
 var i;
 
-// laço de repetição
-// exibir números
+/* laço de repetição para exibir números de 0 a 100 */
 for (i = 0; i <= 100; i++)
 {
     document.write(i + " <br>");
